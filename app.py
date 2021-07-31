30ea0b686dc82cb5d09f4404422d8b628653b94c
ialjumah <66955118+ialjumah@users.noreply.github.com>
2021-07-31 19:05:08 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 30AD-FC05
+
+ Directory of C:\Users\HP\Documents\nd064_course_1-main\solutions\python-helloworld
+
+13/07/2021  14:33    <DIR>          .
+13/07/2021  14:33    <DIR>          ..
+13/07/2021  14:34                 0 app.log
+13/07/2021  14:34                 0 app.py
+10/07/2021  23:21               175 Dockerfile
+13/07/2021  14:33               159 listing
+11/07/2021  13:34               485 requirements.txt
+10/07/2021  23:21                94 test_with_pytest.py
+11/07/2021  00:22    <DIR>          __pycache__
+               6 File(s)            913 bytes
+               3 Dir(s)  42,745,831,424 bytes free
